658fd33f65648429aa53f2b07e17a44dc9927741
ycchen112 <ycchen112@163.com>
2025-05-28 13:50:30 +0800

    Add ros2 code

---
+++ src/reader_vel/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'reader_vel'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools', 'geometry_msgs'],
+    zip_safe=True,
+    maintainer='yc',
+    maintainer_email='yc@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "reader_vel_node = reader_vel.reader_vel:main"
+        ],
+    },
+)
